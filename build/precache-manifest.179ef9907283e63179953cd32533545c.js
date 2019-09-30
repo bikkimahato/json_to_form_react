156f0be50e85a668ad81f4104b7cea39577eea30
bikkimahato <bikkimahato123@gmail.com>
2019-09-30 10:36:15 +0530

    deployed

---
+++ build/precache-manifest.179ef9907283e63179953cd32533545c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb665eac6db64f4068c517ea7a08df7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d26b1693d8bd3ada4e71",
+    "url": "/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "d26b1693d8bd3ada4e71",
+    "url": "/static/js/main.f85318aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
